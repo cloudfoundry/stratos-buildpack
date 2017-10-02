34469f77ad17faf978d3bd18fb90c8f6bef30102
Neil MacDougall <neil.macdougall@suse.com>
2017-10-02 11:36:29 +0100

    Install nodejs and added debug

---
+++ scripts/install_nodejs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -euo pipefail
+
+NODE_VERSION="6.11.3"
+
+export NodeInstallDir="/tmp/node$NODE_VERSION"
+mkdir -p $NodeInstallDir
+
+if [ ! -f $NodeInstallDir/go/bin/go ]; then
+  NODE_MD5="ba51d463da63776d20582e07cb9b4d9d"
+
+  URL=https://buildpacks.cloudfoundry.org/dependencies/node/node-${NODE_VERSION}-linux-x64-ba51d463.tgz
+
+  echo "-----> Download Nodejs ${NODE_VERSION}"
+  curl -s -L --retry 15 --retry-delay 2 $URL -o /tmp/nodejs.tar.gz
+
+  DOWNLOAD_MD5=$(md5sum /tmp/nodejs.tar.gz | cut -d ' ' -f 1)
+
+  if [[ $DOWNLOAD_MD5 != $NODE_MD5 ]]; then
+    echo "       **ERROR** MD5 mismatch: got $DOWNLOAD_MD5 expected $NODE_MD5"
+    exit 1
+  fi
+
+  tar xzf /tmp/nodejs.tar.gz -C $NodeInstallDir
+  rm /tmp/nodejs.tar.gz
+fi
+
+ls -al $NodeInstallDir
+
+#if [ ! -f $NodeInstallDir/go/bin/go ]; then
+#  echo "       **ERROR** Could not download nodejs"
+#  exit 1
+#fi
