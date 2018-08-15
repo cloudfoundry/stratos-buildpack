481ff47f7665f7d3a7da4ef55593fc957a2d1742
Neil MacDougall <neil.macdougall@suse.com>
2018-08-15 15:32:11 +0100

    Add support for dep

---
+++ scripts/install_dep.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+
+# Download dep
+
+DEP_VERSION="0.5.0"
+DEP_PLATFORM="linux-amd64"
+
+export DepInstallDir="/tmp/dep/$DEP_VERSION"
+mkdir -p $DepInstallDir
+
+if [ ! -f $DepInstallDir/dep ]; then
+  URL=https://github.com/golang/dep/releases/download/v${DEP_VERSION}/dep-${DEP_PLATFORM}
+  
+  echo "-----> Download dep ${DEP_VERSION}"
+  curl -s -L --retry 15 --retry-delay 2 $URL -o $DepInstallDir
+fi
+
+export DepDir=$DepInstallDir
+if [ ! -f $DepDir/dep ]; then
+  echo "       **ERROR** Could not download dep"
+  exit 1
+fi
