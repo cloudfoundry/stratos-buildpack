ff15fabd3a4801dc6f1d47dcc76a59971bfb2c8e
Neil MacDougall <neil.macdougall@suse.com>
2018-08-15 15:45:44 +0100

    Ensure dep is executable

--- scripts/install_dep.sh
+++ scripts/install_dep.sh
@@ -13,6 +13,7 @@ if [ ! -f $DepInstallDir/dep ]; then
   URL=https://github.com/golang/dep/releases/download/v${DEP_VERSION}/dep-${DEP_PLATFORM}
   echo "-----> Download dep ${DEP_VERSION}"
   curl -s -L --retry 15 --retry-delay 2 $URL -o $DepInstallDir/dep
+  chmod +x $DepInstallDir/dep
 fi
 
 if [ ! -f $DepInstallDir/dep ]; then
