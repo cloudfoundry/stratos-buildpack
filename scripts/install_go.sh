dae8213123f37b273df7fdb5391e5ad1dfaeeea0
Neil MacDougall <neil.macdougall@suse.com>
2018-08-16 16:07:35 +0100

    Cache downloaded artifacts

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -3,27 +3,41 @@ set -euo pipefail
 
 GO_VERSION="1.9"
 
+DOWNLOAD_FOLDER=${CACHE_DIR}/Downloads
+mkdir -p ${DOWNLOAD_FOLDER}
+DOWNLOAD_FILE=${DOWNLOAD_FOLDER}/go${GO_VERSION}.tar.gz
+
 export GoInstallDir="/tmp/go$GO_VERSION"
 mkdir -p $GoInstallDir
 
-if [ ! -f $GoInstallDir/go/bin/go ]; then
+# Download the archive if we do not have it cached
+if [ ! -f ${DOWNLOAD_FILE} ]; then
+  # Delete any cached go downloads, since those are now out of date
+  rm -rf ${DOWNLOAD_FOLDER}/go*.tar.gz
+
   GO_MD5="4577d9ba083ac86de78012c04a2981be"
   URL=https://buildpacks.cloudfoundry.org/dependencies/go/go${GO_VERSION}.linux-amd64-${GO_MD5:0:8}.tar.gz
 
   echo "-----> Download go ${GO_VERSION}"
-  curl -s -L --retry 15 --retry-delay 2 $URL -o /tmp/go.tar.gz
+  curl -s -L --retry 15 --retry-delay 2 $URL -o ${DOWNLOAD_FILE}
 
-  DOWNLOAD_MD5=$(md5sum /tmp/go.tar.gz | cut -d ' ' -f 1)
+  DOWNLOAD_MD5=$(md5sum ${DOWNLOAD_FILE} | cut -d ' ' -f 1)
 
   if [[ $DOWNLOAD_MD5 != $GO_MD5 ]]; then
     echo "       **ERROR** MD5 mismatch: got $DOWNLOAD_MD5 expected $GO_MD5"
     exit 1
   fi
-
-  tar xzf /tmp/go.tar.gz -C $GoInstallDir
-  rm /tmp/go.tar.gz
+else
+  echo "GO install package available in cache"
 fi
+
+if [ ! -f $GoInstallDir/go/bin/go ]; then
+  tar xzf ${DOWNLOAD_FILE} -C $GoInstallDir
+fi
+
 if [ ! -f $GoInstallDir/go/bin/go ]; then
   echo "       **ERROR** Could not download go"
   exit 1
 fi
+
+
--- scripts/install_nodejs.sh
+++ scripts/install_nodejs.sh
@@ -3,28 +3,38 @@ set -euo pipefail
 
 NODE_VERSION="8.9.4"
 
+DOWNLOAD_FOLDER=${CACHE_DIR}/Downloads
+mkdir -p ${DOWNLOAD_FOLDER}
+DOWNLOAD_FILE=${DOWNLOAD_FOLDER}/node${NODE_VERSION}.tar.gz
+
 export NodeInstallDir="/tmp/node$NODE_VERSION"
 export NodeDir="$NodeInstallDir/node-v${NODE_VERSION}-linux-x64"
 
 mkdir -p $NodeInstallDir
 
-if [ ! -f  $NodeDir/bin/node ]; then
-  NODE_MD5="5bda713bd4aa39394536fc48c744854b"
+# Download the archive if we do not have it cached
+if [ ! -f ${DOWNLOAD_FILE} ]; then
+  # Delete any cached node downloads, since those are now out of date
+  rm -rf ${DOWNLOAD_FOLDER}/node*.tar.gz
 
+  NODE_MD5="5bda713bd4aa39394536fc48c744854b"
   URL=https://buildpacks.cloudfoundry.org/dependencies/node/node-8.9.4-linux-x64-40e8e080.tgz
 
   echo "-----> Download Nodejs ${NODE_VERSION}"
-  curl -s -L --retry 15 --retry-delay 2 $URL -o /tmp/nodejs.tar.gz
+  curl -s -L --retry 15 --retry-delay 2 $URL -o ${DOWNLOAD_FILE}
 
-  DOWNLOAD_MD5=$(md5sum /tmp/nodejs.tar.gz | cut -d ' ' -f 1)
+  DOWNLOAD_MD5=$(md5sum ${DOWNLOAD_FILE} | cut -d ' ' -f 1)
 
   if [[ $DOWNLOAD_MD5 != $NODE_MD5 ]]; then
     echo "       **ERROR** MD5 mismatch: got $DOWNLOAD_MD5 expected $NODE_MD5"
     exit 1
   fi
+else
+  echo "NodeJS install package available in cache"
+fi
 
-  tar xzf /tmp/nodejs.tar.gz -C $NodeInstallDir
-  rm /tmp/nodejs.tar.gz
+if [ ! -f $NodeDir/bin/node ]; then
+  tar xzf ${DOWNLOAD_FILE} -C $NodeInstallDir
 fi
 
 if [ ! -f $NodeDir/bin/node ]; then
