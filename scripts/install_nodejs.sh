0d529c4ab6686a90191d2413470484df7f77c31e
Neil MacDougall <neil.macdougall@suse.com>
2020-04-14 16:33:50 +0100

    Update

--- scripts/install_nodejs.sh
+++ scripts/install_nodejs.sh
@@ -18,7 +18,7 @@ if [ ! -f ${DOWNLOAD_FILE} ]; then
   rm -rf ${DOWNLOAD_FOLDER}/node*.tar.gz
 
   NODE_SHA256="5c951ab4392e60491cef7687f997a45ef2e9848d84409d10fd8b9ee9ba52232b"
-  URL=https://buildpacks.cloudfoundry.org/dependencies/node/node-13.12.0-linux-x64-cflinuxfs3-5c951ab4.tgz
+  URL=https://buildpacks.cloudfoundry.org/dependencies/node/node_13.12.0_linux_x64_cflinuxfs3_5c951ab4.tgz
   
   echo "-----> Download Nodejs ${NODE_VERSION}"
   curl -s -L --retry 15 --retry-delay 2 $URL -o ${DOWNLOAD_FILE}
@@ -26,7 +26,7 @@ if [ ! -f ${DOWNLOAD_FILE} ]; then
   DOWNLOAD_SHA256=$(shasum -a 256 ${DOWNLOAD_FILE} | cut -d ' ' -f 1)  
 
   if [[ $DOWNLOAD_SHA256 != $NODE_SHA256 ]]; then
-    echo "       **ERROR** SHA256 mismatch: got $DOWNLODOWNLOAD_SHA256AD_MD5 expected $NODE_SHA256"
+    echo "       **ERROR** SHA256 mismatch: got $DOWNLOAD_SHA256 expected $NODE_SHA256"
     exit 1
   fi
 else
